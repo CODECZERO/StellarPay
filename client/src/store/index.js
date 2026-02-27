621a0a729e208eb9a645a19dc42ab478b77d0eb8
CODECZERO <as8857981@gmail.com>
2026-02-27 17:45:43 +0530

    refactor(client): replace redux with zustand store for simplified state management

--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import employeeReducer from "../slices/employeeSlices.js"
-
-export const store=configureStore({
-    reducre:{
-        employee:employeeReducer,
-    },
-});
-
